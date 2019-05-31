ae9cbd8e8eef1cd580faea0bd171b0b9f5d7bd7b
Timz Owen <34624703+TimzOwen@users.noreply.github.com>
2019-05-31 05:36:38 -0400

    Update MainActivity.java
    firebase data for collection added

--- app/src/main/java/com/owen/realtimedatabas/MainActivity.java
+++ app/src/main/java/com/owen/realtimedatabas/MainActivity.java
@@ -12,6 +12,10 @@ public class MainActivity extends AppCompatActivity {
     private ImageView imageView;
     private ProgressBar mProgressBar;
     private EditText mEditText;
+    
+     private DatabaseReference mDataRef;
+    private StorageReference mStorageRef;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         
         findIds();
+        
+        mStorageRef = FirebaseStorage.getInstance().getReference("uploads");
+        mDataRef = FirebaseDatabase.getInstance().getReference("uploads");
 
         btnChooseFile.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,5 +67,25 @@ public class MainActivity extends AppCompatActivity {
         mProgressBar = (ProgressBar)findViewById(R.id.progressBar);
         imageView = (ImageView)findViewById(R.id.iv_imageView);
     }
+    }
+ //create a function to check the file type upload
+    private String getFileExtension(Uri uri)
+    {
+        ContentResolver cR = getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(cR.getType(uri));
+    }
+
+    public void uploadFile()
+    {
+        if (mImageUri != null)
+        {
+
+        }
+        else
+        {
+            Toast.makeText(MainActivity.this, "Please select Image", Toast.LENGTH_LONG).show();
+        }
+
     }
 }
